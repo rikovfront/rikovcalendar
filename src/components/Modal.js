2d2de071a87aa3476fddd3ffb5738fe437daeea0
Vadim <rikovfront@gmail.com>
2022-12-09 17:05:50 +0400

    gh-pages

--- src/components/Modal.js
+++
@@ -1,8 +0,0 @@
-export function Modal({ submodal, style, children }) {
-  return (
-    <>
-      <div className="overlay"></div>
-      <div className={submodal ? 'submodal-container' : 'modal-container'} style={style}>{children}</div>
-    </>
-  );
-}
